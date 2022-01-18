86e125211ebd1eb33e4af48133c67dd5cbed2f24
]Daniyaal Rasheed <drasheed@purdue.edu>
2022-01-18 02:37:53 -0500

    Completed register_file

---
+++ source/register_file.sv
@@ -0,0 +1,35 @@
+`include "cpu_types_pkg.vh"
+`include "register_file_if.vh"
+
+import cpu_types_pkg::*;
+
+module register_file (
+    input CLK, nRST,
+    register_file_if.rf rfif
+);
+
+word_t registers[2**REG_W];
+word_t nxt_registers[2**REG_W];
+
+always_ff @ (posedge CLK, negedge nRST) 
+begin
+    if (nRST == 1'b0)
+        for (int i = 0; i < 2**REG_W; i++)
+        begin
+            registers[i] <= '0;
+        end
+    else
+        registers <= nxt_registers;
+end
+
+always_comb
+begin
+    nxt_registers = registers;
+    if (rfif.WEN && rfif.wsel != '0)
+            nxt_registers[rfif.wsel] = rfif.wdat;
+end
+
+assign rfif.rdat1 = registers[rfif.rsel1];
+assign rfif.rdat2 = registers[rfif.rsel2];
+
+endmodule
--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -6,7 +6,9 @@
 */
 
 // mapped needs this
+`include "cpu_types_pkg.vh"
 `include "register_file_if.vh"
+import cpu_types_pkg::*;
 
 // mapped timing needs this. 1ns is too fast
 `timescale 1 ns / 1 ns
@@ -28,7 +30,7 @@ module register_file_tb;
   // interface
   register_file_if rfif ();
   // test program
-  test PROG ();
+  test PROG (CLK, nRST, rfif);
   // DUT
 `ifndef MAPPED
   register_file DUT(CLK, nRST, rfif);
@@ -48,5 +50,116 @@ module register_file_tb;
 
 endmodule
 
-program test;
+program test(input CLK, output logic nRST, register_file_if.tb rfif);
+  parameter CHECK_DELAY = 5ns;
+
+  logic rdat1_success;
+  logic rdat2_success;
+  word_t expected_registers[2**REG_W];
+
+  task reset;
+  begin
+    rdat1_success = 1'b1;
+    rdat2_success = 1'b1;
+
+    rfif.rsel1 = '0;
+    rfif.rsel2 = '0;
+    rfif.wsel = '0;
+    rfif.wdat = '0;
+    rfif.WEN = 0;
+    reset_expected_registers();
+
+    @(negedge CLK);
+    nRST = 1'b0;
+    #(register_file_tb.PERIOD * 2);
+    nRST = 1'b1;
+    #(register_file_tb.PERIOD * 2);
+  end
+  endtask
+
+  task reset_expected_registers;
+  begin
+    for (int i = 0; i < $size(expected_registers); i++)
+    begin
+      expected_registers[i] = '0;
+    end
+  end
+  endtask
+
+  task check_output;
+  begin
+    fork
+      for (int i = 0; i < $size(expected_registers); i++)
+      begin
+        @(negedge CLK);
+        rfif.rsel1 = i;
+        @(posedge CLK);
+        @(negedge CLK);
+        if (rfif.rdat1 != expected_registers[i])
+          rdat1_success = 1'b0;
+      end
+
+      for (int i = 0; i < $size(expected_registers); i++)
+      begin
+        @(negedge CLK);
+        rfif.rsel2 = i;
+        @(posedge CLK);
+        @(negedge CLK);
+        if (rfif.rdat2 != expected_registers[i])
+          rdat2_success = 1'b0;
+      end
+    join
+  end
+  endtask
+
+  task write_reg;
+    input int i;
+    input word_t data;
+  begin
+    if (i != 0)
+      expected_registers[i] = data;
+    
+    @(negedge CLK);
+    rfif.WEN = 1'b1;
+    rfif.wsel = i;
+    rfif.wdat = data;
+    @(posedge CLK);
+    @(negedge CLK);
+    rfif.wsel = 1'b0;
+    rfif.wsel = '0;
+    rfif.wdat = '0;
+  end
+  endtask
+
+  initial
+  begin
+    reset();
+    check_output();
+
+    // basic write and read
+    reset();
+    write_reg(1, 20);
+    check_output();
+
+    // write to 0 and read
+    reset();
+    write_reg(0, 20);
+    check_output();
+
+    // basic write to all and read
+    reset();
+    for (int i = 0; i < $size(expected_registers); i++)
+    begin
+      write_reg(i, i);
+    end
+    check_output();
+
+    // complex
+    reset();
+    for (int i = 0; i < $size(expected_registers); i++)
+    begin
+      write_reg(i, 1234567*i);
+    end
+    check_output();
+  end
 endprogram
