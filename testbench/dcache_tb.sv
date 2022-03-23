eb3e46d8071e60b0c28732351fe66d7bcf8972fd
Isaac Hagedorn <ihagedo@purdue.edu>
2022-03-23 11:06:50 -0400

    plugged in icache and dcache

--- testbench/dcache_tb.sv
+++ testbench/dcache_tb.sv
@@ -1,5 +1,7 @@
 `include "caches_if.vh"
 `include "datapath_cache_if.vh"
+`include "cache_control_if.vh"
+`include "cpu_ram_if.vh"
 `include "cpu_types_pkg.vh"
 import cpu_types_pkg::*;
 
@@ -14,6 +16,9 @@ module dcache_tb;
 
   caches_if cif ();
   datapath_cache_if dcif ();
+  cpu_ram_if prif ();
+  caches_if cc0 ();
+  cache_control_if #(.CPUS(1)) ccif (cif, cc0);
   
   test PROG (CLK, nRST, cif, dcif);
 
@@ -48,6 +53,16 @@ module dcache_tb;
   );
 `endif
 
+  ram RAM (CLK, nRST, prif);
+  assign ccif.ramload = prif.ramload;
+  assign ccif.ramstate = prif.ramstate;
+  assign prif.ramREN = ccif.ramREN;
+  assign prif.ramWEN = ccif.ramWEN;
+  assign prif.ramaddr = ccif.ramaddr;
+  assign prif.ramstore = ccif.ramstore;
+
+  memory_control MEM (CLK, nRST, ccif);
+
 endmodule
 
 program test
@@ -74,16 +89,16 @@ task set_dp_values;
 	end
 endtask
 
-task set_mc_values;
+/*task set_mc_values;
 	input logic dwait_tb;
 	input word_t dload_tb;
 	begin
 		tbc.dload = dload_tb;
 		tbc.dwait = dwait_tb;
 	end
-endtask
+endtask*/
 
-task check_values1;
+/*task check_values1;
 	input logic expected_dren, expected_dwen;
 	input word_t expected_daddr, expected_dstore;
 	begin		
@@ -123,7 +138,7 @@ task check_values1;
 			$display("Test Case #%0d Instr #%0d, WEN ERROR", test_case_num, instr_num);
 		end
 	end
-endtask
+endtask*/
 
 task check_values2;
 	input logic expected_dhit;
@@ -157,13 +172,8 @@ initial begin
 	tbdc.imemREN = 1;
 	tbdc.datomic = 0;
 	tbdc.imemaddr = 0;
-	tbc.ccwait = 0;
-	tbc.ccinv = 0;
-	tbc.ccsnoopaddr = 0;
-	tbc.iwait = 1;
-	tbc.iload = 0;
 	set_dp_values(0, 0, 32'h0, 32'h0, 0);
-	set_mc_values(1, 32'h0);
+	//set_mc_values(1, 32'h0);
 
 	nRST = 0;
 	#(2*PERIOD);
@@ -172,55 +182,56 @@ initial begin
 	//load to all in frame0
 	//load from 0x80 miss
 	set_dp_values(1, 0, {30'h80, 2'b00}, 32'h0, 0);
-	set_mc_values(1, 32'h0);
+	//set_mc_values(1, 32'h0);
+	#(3*PERIOD);
+	//set_mc_values(0, 32'h12345678);
+	#(3*PERIOD);
+	//set_mc_values(1, 32'h0);
 	#(PERIOD);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
-	set_mc_values(1, 32'h0);
-	#(PERIOD);
-	set_mc_values(0, 32'h87654321);
-	#(PERIOD);
+	//set_mc_values(1, 32'h0);
 	//load from 0xA2 miss
 	set_dp_values(1, 0, {30'hA2, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0xC4 miss
 	set_dp_values(1, 0, {30'hC4, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x106 miss
 	set_dp_values(1, 0, {30'h106, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x148 miss
 	set_dp_values(1, 0, {30'h148, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x18A miss
 	set_dp_values(1, 0, {30'h18A, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x1AC miss
 	set_dp_values(1, 0, {30'h1AC, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x24E miss
 	set_dp_values(1, 0, {30'h24E, 2'b00}, 32'h0, 0);
-	set_mc_values(0, 32'h12345678);
+	//set_mc_values(0, 32'h12345678);
 	#(2*PERIOD);
-	set_mc_values(0, 32'h87654321);
+	//set_mc_values(0, 32'h87654321);
 	#(PERIOD);
 	//load from 0x81 hit
 	set_dp_values(1, 0, {30'h81, 2'b00}, 32'h0, 0);
@@ -247,9 +258,58 @@ initial begin
 	set_dp_values(1, 0, {30'h24F, 2'b00}, 32'h0, 0);
 	#(2*PERIOD);
 
+	//store cases
+	//store into 0x80 dirty hit
+	set_dp_values(0, 1, {30'h80, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x81 dirty hit
+	set_dp_values(0, 1, {30'h81, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0xA2 dirty hit
+	set_dp_values(0, 1, {30'hA2, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0xA3 dirty hit
+	set_dp_values(0, 1, {30'hA3, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0xC4 dirty hit
+	set_dp_values(0, 1, {30'hC4, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0xC5 dirty hit
+	set_dp_values(0, 1, {30'hC5, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0x106 dirty hit
+	set_dp_values(0, 1, {30'h106, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x107 dirty hit
+	set_dp_values(0, 1, {30'h107, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0x148 dirty hit
+	set_dp_values(0, 1, {30'h148, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x149 dirty hit
+	set_dp_values(0, 1, {30'h149, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0x18A dirty hit
+	set_dp_values(0, 1, {30'h18A, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x18B dirty hit
+	set_dp_values(0, 1, {30'h18B, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0x1AC dirty hit
+	set_dp_values(0, 1, {30'h1AC, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x1AD dirty hit
+	set_dp_values(0, 1, {30'h1AD, 2'b00}, 32'ha5a55a5a, 0);
+	#(2*PERIOD);
+	//store into 0x24E dirty hit
+	set_dp_values(0, 1, {30'h24E, 2'b00}, 32'h5a5aa5a5, 0);
+	#(2*PERIOD);
+	//store into 0x24F dirty hit
+	set_dp_values(0, 1, {30'h24F, 2'b00}, 32'ha5a55a5a, 0);
+
 	//load to all in frame1
 	//load from 0x3fffffff miss
-	set_dp_values(1, 0, {30'h3fffffff, 2'b00}, 32'h0, 0);
+	/*set_dp_values(1, 0, {30'h3fffffff, 2'b00}, 32'h0, 0);
 	set_mc_values(0, 32'hDEAFBEEF);
 	#(2*PERIOD);
 	set_mc_values(1, 32'h0);
@@ -396,7 +456,7 @@ initial begin
 	//halt
 	set_mc_values(1, 32'h0);
 	set_dp_values(0, 0, 32'h0, 32'h0, 1);
-
+	*/
 	#(PERIOD*10);
 end
 
