e8499179537d5189325c02ee41b13a8d2e593683
Isaac Hagedorn <ihagedo@purdue.edu>
2022-03-24 20:13:33 -0400

    changed dcache2 range

--- asmFiles/test.dcache2.asm
+++ asmFiles/test.dcache2.asm
@@ -1,62 +1,54 @@
 org 0x0000
-  ori   $10,$zero,0xF0000
-  ori   $11,$zero,0x108
-  ori   $12,$zero,0x510
-  ori   $13,$zero,0xA18
-  ori   $14,$zero,0xBA0
-  ori   $15,$zero,0xCA8
-  ori   $16,$zero,0xDB0
-  ori   $17,$zero,0xEB8
+  ori   $4,$zero,0xF000
+  ori   $5,$zero,0x108
+  ori   $6,$zero,0x510
+  ori   $7,$zero,0xA18
+  ori   $8,$zero,0xBA0
+  ori   $9,$zero,0xCA8
+  ori   $10,$zero,0xDB0
+  ori   $11,$zero,0xEB8
 
-  addu $21, $zero, $10
+  lw $1, 0($4)
+  lw $2, 4($4)
+  lw $3, 8($4)
 
-  lw $1, 0($21)
-  lw $2, 4($21)
-  lw $3, 8($21)
-
-  addu $21, $10, $11
+  add $21, $4, $5
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $12
+  add $21, $4, $6
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $12
+  add $21, $4, $7
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $13
+  add $21, $4, $8
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $14
+  add $21, $4, $9
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $15
+  add $21, $4, $10
 
   lw $1, 0($21)
   lw $2, 4($21)
   lw $3, 8($21)
 
-  addu $21, $10, $16
-
-  lw $1, 0($21)
-  lw $2, 4($21)
-  lw $3, 8($21)
-
-  addu $21, $10, $17
+  add $21, $4, $11
 
   lw $1, 0($21)
   lw $2, 4($21)
@@ -64,42 +56,42 @@ org 0x0000
 
   halt
 
-org   0xF0000
+org   0xF000
   cfw   0x1331
   cfw   0x4242
   cfw   0x2332
 
-org   0xF0108
+org   0xF108
   cfw   0x7337
   cfw   0x2701
   cfw   0x1337
 
-org   0xF0510
+org   0xF510
   cfw   0x1331
   cfw   0x4242
   cfw   0x2332
 
-org   0xF0A18
+org   0xFA18
   cfw   0x7337
   cfw   0x2701
   cfw   0x1337
 
-org   0xF0BA0
+org   0xFBA0
   cfw   0x1331
   cfw   0x4242
   cfw   0x2332
 
-org   0xF0CA8
+org   0xFCA8
   cfw   0x7337
   cfw   0x2701
   cfw   0x1337
 
-org   0xF0DB0
+org   0xFDB0
   cfw   0x1331
   cfw   0x4242
   cfw   0x2332
 
-org   0xF0EB8
+org   0xFEB8
   cfw   0x7337
   cfw   0x2701
   cfw   0x1337
